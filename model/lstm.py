70468274e506e8510400a8a1ff68f28fc2363db7
Seleucia <coskunh1986@gmail.com>
2016-10-18 16:26:40 +0300

    My commit msg

---
+++ model/lstm.py
@@ -0,0 +1,61 @@
+import numpy as np
+import theano
+from layers import LSTMLayer,DropoutLayer
+import theano.tensor as T
+from theano import shared
+from helper.utils import init_weight,init_bias,get_err_fn,count_params
+from helper.optimizer import RMSprop
+
+dtype = T.config.floatX
+
+class lstm:
+   def __init__(self,rng, params,cost_function='mse',optimizer = RMSprop):
+       batch_size=params['batch_size']
+       sequence_length=params["seq_length"]
+
+       lr=params['lr']
+       self.n_in = 2048
+       self.n_lstm = params['n_hidden']
+       self.n_out = params['n_output']
+
+       self.W_hy = init_weight((self.n_lstm, self.n_out), rng=rng,name='W_hy', sample= 'glorot')
+       self.b_y = init_bias(self.n_out,rng=rng, sample='zero')
+
+       layer1=LSTMLayer(rng,0,self.n_in,self.n_lstm)
+
+       self.params = layer1.params
+       self.params.append(self.W_hy)
+       self.params.append(self.b_y)
+
+       def step_lstm(x_t,h_tm1_1,c_tm1_1):
+           [h_t_1,c_t_1,y_t_1]=layer1.run(x_t,h_tm1_1,c_tm1_1)
+           y = T.dot(y_t_1, self.W_hy) + self.b_y
+           return [h_t_1,c_t_1,y]
+
+       X = T.tensor3() # batch of sequence of vector
+       Y = T.tensor3() # batch of sequence of vector
+       is_train = T.iscalar('is_train') # pseudo boolean for switching between training and prediction
+       H = T.matrix(name="H",dtype=dtype) # initial hidden state
+       C = T.matrix(name="C",dtype=dtype) # initial hidden state
+
+       noise= rng.normal(size=(batch_size,sequence_length,self.n_in), std=0.0002, avg=0.0,dtype=theano.config.floatX)
+       X_train=noise+X
+
+       X_tilde= T.switch(T.neq(is_train, 0), X_train, X)
+
+       [h_t_1,c_t_1,y_vals], _ = theano.scan(fn=step_lstm,
+                                         sequences=[X_tilde.dimshuffle(1,0,2)],
+                                         outputs_info=[H, C, None])
+
+       self.output = y_vals.dimshuffle(1,0,2)
+       cost=get_err_fn(self,cost_function,Y)
+
+       _optimizer = optimizer(
+            cost,
+            self.params,
+            lr=lr
+        )
+
+       self.train = theano.function(inputs=[X,Y,is_train,H,C],outputs=[cost,h_t_1[-1],c_t_1[-1]],updates=_optimizer.getUpdates(),allow_input_downcast=True)
+       self.predictions = theano.function(inputs = [X,is_train,H,C], outputs = [self.output,h_t_1[-1],c_t_1[-1]],allow_input_downcast=True)
+       self.n_param=count_params(self.params)
--- model/model_provider.py
+++ model/model_provider.py
@@ -1,82 +1,13 @@
-
-from model.lstm import  lstm
-from model.cnn_lstm import  cnn_lstm_s
-from model.lstm2erd import lstm2erd
-from model.gru import gru
 from model.cnn_lstm import cnn_lstm
-from model.cnn import cnn
-from model.cnn2 import cnn2
-from model.cnn3 import cnn3
-from model.cnn4 import cnn4
-from model.cnn5 import cnn5
-from model.cnn6 import cnn6
-from model.cnn7 import cnn7
-from model.cnn8 import cnn8
-from model.cnn9 import cnn9
-from model.autoencoder import autoencoder
-from model.cnn_decoder import cnn_decoder
-from model.lstm_auto import lstm_auto
-from model.lstm_joints import lstm_joints
-from model.lstm_mdn import lstm_mdn
-from model.lstm_3layer_mdn import lstm_3layer_mdn
-from model.lstm_3layer_joints import lstm_3layer_joints
-from model.lstm_3layer_joints2 import lstm_3layer_joints2
-from model.cnn_lstm_auto import cnn_lstm_auto
-from model.lstm_auto_3layer import lstm_auto_3layer
-from model.lstm_skelton import lstm_skelton
-from model.real_rcnn import real_rcnn
+from model.lstm import lstm
 from helper.optimizer import ClipRMSprop, RMSprop,Adam,AdamClip
 import helper.utils as u
 
 def get_model(params,rng):
     if(params["model"]=="lstm"):
         model = lstm(rng,params, optimizer=Adam)
-    elif(params["model"]=="cnn_lstm_s"):
-        model = cnn_lstm_s(rng,params, optimizer=Adam)
     elif(params["model"]=="cnn_lstm"):
-        model = cnn_lstm(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="real_rcnn"):
-        model = real_rcnn(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="cnn"):
-        model = cnn(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="cnn2"):
-        model = cnn2(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="cnn3"):
-        model = cnn3(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="cnn4"):
-        model = cnn4(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="cnn5"):
-        model = cnn5(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="cnn6"):
-        model = cnn6(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="cnn7"):
-        model = cnn7(rng=rng,params=params,optimizer=AdamClip)
-    elif(params["model"]=="cnn8"):
-        model = cnn8(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="cnn9"):
-        model = cnn9(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="autoencoder"):
-        model = autoencoder(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="cnn_decoder"):
-        model = cnn_decoder(rng=rng,params=params,optimizer=Adam)
-    elif(params["model"]=="lstm_auto"):
-        model = lstm_auto(rng=rng,params=params,optimizer=AdamClip)
-    elif(params["model"]=="lstm_joints"):
-        model = lstm_joints(rng=rng,params=params,optimizer=AdamClip)
-    elif(params["model"]=="lstm_3layer_joints"):
-        model = lstm_3layer_joints(rng=rng,params=params,optimizer=AdamClip)
-    elif(params["model"]=="lstm_3layer_joints2"):
-        model = lstm_3layer_joints2(rng=rng,params=params,optimizer=AdamClip)
-    elif(params["model"]=="cnn_lstm_auto"):
-        model = cnn_lstm_auto(rng=rng,params=params,optimizer=AdamClip)
-    elif(params["model"]=="lstm_auto_3layer"):
-        model = lstm_auto_3layer(rng=rng,params=params,optimizer=AdamClip)
-    elif(params["model"]=="lstm_skelton"):
-        model = lstm_skelton(rng=rng,params=params,optimizer=AdamClip)
-    elif(params["model"]=="lstm_mdn"):
-        model = lstm_mdn(rng=rng,params=params,optimizer=AdamClip)
-    elif(params["model"]=="lstm_3layer_mdn"):
-        model = lstm_3layer_mdn(rng=rng,params=params,optimizer=AdamClip)
+        model = cnn_lstm(rng,params, optimizer=Adam)
     else:
         raise Exception('Wrong model calling....') #
     return model
